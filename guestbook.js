d444387ffd055da4d2e99bee04fd6ecd658ba36f
Victor Torres <vitugo7@hotmail.com>
2022-03-31 22:10:15 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://f2dd2fb9.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
